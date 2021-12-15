a607c70372b66c71fe56469656d1a09e74f5b2e0
ducngai <96194229+ducngai@users.noreply.github.com>
2021-12-15 22:51:27 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>COuntdown-Javascript</title>
+</head>
+<body>
+    <div class="item">
+        <p>DAYS</p>
+        <span id="days">Days</span>
+    </div>
+    <div class="item">
+        <p>HOURS</p>
+        <span id="hours">Hours</span>
+    </div>
+    <div class="item">
+        <p>MINUTES</p>
+        <span id="minutes">Minutes</span>
+    </div>
+    <div class="item">
+        <p>SECONDS</p>
+        <span id="seconds">Seconds</span>
+    </div>
+</body>
+<script src="script.js"></script>
+</html>
